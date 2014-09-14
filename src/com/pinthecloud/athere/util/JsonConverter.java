0d0640fb7cdd3bdc345e85c5363eafcda95a7ced
hongkunyoo <hongkunyoo@gmail.com>
2014-09-15 00:27:41 +0900

    Improved JsonConverter

--- src/com/pinthecloud/athere/util/JsonConverter.java
+++ src/com/pinthecloud/athere/util/JsonConverter.java
@@ -2,39 +2,62 @@ package com.pinthecloud.athere.util;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.pinthecloud.athere.model.AhUser;
 import com.pinthecloud.athere.model.Square;
 
 public class JsonConverter {
-	private static JsonElement getJsonElement(JsonObject jo, String key) {
+	private enum TYPE {
+		STRING,
+		DOUBLE,
+		INT,
+		BOOLEAN
+	}
+	private static JsonElement getDefaultValue(TYPE type) {
+		String jsonString = "";
+		
+		switch(type) {
+		case STRING : break;
+		case DOUBLE : jsonString = "0.0"; break;
+		case INT : jsonString = "0"; break;
+		case BOOLEAN : jsonString = "false"; break;
+		default : break;
+		}
+		
+		return new JsonParser().parse(jsonString);
+	}
+	
+	private static JsonElement getValue(JsonObject jo, String key, TYPE type) {
 		JsonElement je = jo.get(key);
-		if (je == null) je = jo.get(key.toLowerCase(Locale.US));
+		if (je == null) return getDefaultValue(type);
 		return je;
 	}
+	
+//	private static JsonElement getJsonElement(JsonObject jo, String key) {
+//		JsonElement je = jo.get(key);
+//		if (je == null) je = jo.get(key.toLowerCase(Locale.US));
+//		return je;
+//	}
 	public static List<Square> convertToSquareList(JsonArray jsonArray) {
 		List<Square> list = new ArrayList<Square>();
 
 		for (int i = 0 ; i < jsonArray.size() ; i++) {
 			JsonElement jsonElement = jsonArray.get(i);
 			JsonObject jo = jsonElement.getAsJsonObject();
-			String id = jo.get("id").getAsString();
-			String name = jo.get("name").getAsString();
-			double latitude = jo.get("latitude").getAsDouble();
-			double longitude = jo.get("longitude").getAsDouble();
-			String whoMade = jo.get("whoMade").getAsString();
-			int maleNum = getJsonElement(jo, "maleNum").getAsInt();
-			int femaleNum = getJsonElement(jo, "femaleNum").getAsInt();
-			int distance = jo.get("distance").getAsInt();
-			boolean isAdmin = jo.get("isAdmin").getAsBoolean();
-			String code = "";
-			if(isAdmin){
-				code = jo.get("code").getAsString();
-			}
+			String id = getValue(jo, ("id"), TYPE.STRING).getAsString();
+			String name = getValue(jo, ("name"), TYPE.STRING).getAsString();
+			double latitude = getValue(jo, ("latitude"), TYPE.DOUBLE).getAsDouble();
+			double longitude = getValue(jo, ("longitude"), TYPE.DOUBLE).getAsDouble();
+			String whoMade = getValue(jo, ("whoMade"), TYPE.STRING).getAsString();
+			int maleNum = getValue(jo, "maleNum", TYPE.INT).getAsInt();
+			int femaleNum = getValue(jo, "femaleNum", TYPE.INT).getAsInt();
+			int distance = getValue(jo, ("distance"), TYPE.INT).getAsInt();
+			boolean isAdmin = getValue(jo, ("isAdmin"), TYPE.BOOLEAN).getAsBoolean();
+			String code = getValue(jo, ("code"), TYPE.STRING).getAsString();
 
 			Square square = new Square();
 			square.setId(id);
@@ -63,17 +86,17 @@ public class JsonConverter {
 			JsonElement jsonElement = jsonArray.get(i);
 			JsonObject jo = jsonElement.getAsJsonObject();
 			AhUser user = new AhUser();
-			user.setId(jo.get("id").getAsString());
-			user.setNickName(jo.get("nickName").getAsString());
-			user.setProfilePic(jo.get("profilePic").getAsString());
-			user.setMobileId(jo.get("mobileId").getAsString());
-			user.setRegistrationId(jo.get("registrationId").getAsString());
-			user.setMale(jo.get("isMale").getAsBoolean());
-			user.setCompanyNum(jo.get("companyNum").getAsInt());
-			user.setAge(jo.get("age").getAsInt());
-			user.setSquareId(jo.get("squareId").getAsString());
+			user.setId(getValue(jo, ("id"), TYPE.STRING).getAsString());
+			user.setNickName(getValue(jo, ("nickName"), TYPE.STRING).getAsString());
+			user.setProfilePic(getValue(jo, ("profilePic"), TYPE.STRING).getAsString());
+			user.setMobileId(getValue(jo, ("mobileId"), TYPE.STRING).getAsString());
+			user.setRegistrationId(getValue(jo, ("registrationId"), TYPE.STRING).getAsString());
+			user.setMale(getValue(jo, ("isMale"), TYPE.BOOLEAN).getAsBoolean());
+			user.setCompanyNum(getValue(jo, ("companyNum"), TYPE.INT).getAsInt());
+			user.setAge(getValue(jo, ("age"), TYPE.INT).getAsInt());
+			user.setSquareId(getValue(jo, ("squareId"), TYPE.STRING).getAsString());
 			//			user.setChatEnable(jo.get("isChatEnable").getAsBoolean());
-			user.setChupaEnable(jo.get("isChupaEnable").getAsBoolean());
+			user.setChupaEnable(getValue(jo, ("isChupaEnable"), TYPE.BOOLEAN).getAsBoolean());
 
 			list.add(user);
 		}
